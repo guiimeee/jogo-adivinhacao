859b9f61656abda4c3fb8b7d1b6c4f68aeaceba7
guiimeee <guilhermeilhaer@gmail.com>
2025-02-12 11:30:00 -0300

    random number gerado ao clicar em reset

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
-const randomNumber = Math.round(Math.random() * 10)
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
-function handleClick(event) {
+// função callback
+function handleTryClick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector("#inputNumber")
 
   if(Number(inputNumber.value) == randomNumber) {
-    document.querySelector(".screen1").classList.add("hide")
-    document.querySelector(".screen2").classList.remove("hide")
+    screen1.classList.add("hide")
+    screen2.classList.remove("hide")
 
-    document.querySelector(".screen2 h2").innerText = `Você acertou em ${xAttempts} Tentativas`
+    screen2.querySelector("h2").innerText = `Você acertou em ${xAttempts} Tentativas`
   }
 
+  inputNumber.value = ""
   xAttempts++
 }
+
+
+// Eventos
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', function() {
+  screen1.classList.remove("hide")
+  screen2.classList.add("hide")
+  xAttempts = 1
+  randomNumber = Math.round(Math.random() * 10)
+})
